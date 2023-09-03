8e100e1e634be289eaec9ae36a65d02b1993df6a
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-09-03 08:42:45 +0530

    created AboutUsFragment.java in main package

---
+++ app/src/main/java/com/uniquestudio/magicmaths/ui/main/AboutUsFragment.java
@@ -0,0 +1,51 @@
+package com.uniquestudio.magicmaths.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.uniquestudio.magicmaths.R;
+
+public class AboutUsFragment extends Fragment {
+
+
+
+    private MainViewModel mViewModel;
+
+    public static AboutUsFragment newInstance() {
+        return new AboutUsFragment();
+    }
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment2, container, false);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+
+
+    }
+
+
+
+
+}
