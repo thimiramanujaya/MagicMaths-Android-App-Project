197099f11629243afbdd7767ab043aa6c6d7f6a8
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-09-03 08:41:42 +0530

    created HelpFragment.java in main package

---
+++ app/src/main/java/com/uniquestudio/magicmaths/ui/main/HelpFragment.java
@@ -0,0 +1,39 @@
+package com.uniquestudio.magicmaths.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uniquestudio.magicmaths.R;
+
+public class HelpFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static HelpFragment newInstance() {
+        return new HelpFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
